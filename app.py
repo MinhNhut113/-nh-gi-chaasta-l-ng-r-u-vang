6c7187026e4dfd8cebf3daa82b14686c0b8af133
MinhNhut113 <nhuthoclaptrinh1173@gmail.com>
2025-07-02 00:55:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -24,17 +24,17 @@ Dựa trên các chỉ số hóa học để dự đoán điểm chất lượng
 """)
 
 # Tạo các input cho người dùng nhập
-fixed_acidity = st.number_input("Fixed Acidity", 0.0, 20.0, 7.4)
-volatile_acidity = st.number_input("Volatile Acidity", 0.0, 2.0, 0.7)
-citric_acid = st.number_input("Citric Acid", 0.0, 1.0, 0.0)
-residual_sugar = st.number_input("Residual Sugar", 0.0, 15.0, 1.9)
-chlorides = st.number_input("Chlorides", 0.0, 0.2, 0.076)
-free_sulfur_dioxide = st.number_input("Free Sulfur Dioxide", 0.0, 80.0, 11.0)
-total_sulfur_dioxide = st.number_input("Total Sulfur Dioxide", 0.0, 300.0, 34.0)
-density = st.number_input("Density", 0.9900, 1.0050, 0.9978)
-pH = st.number_input("pH", 2.5, 4.5, 3.51)
-sulphates = st.number_input("Sulphates", 0.3, 2.0, 0.56)
-alcohol = st.number_input("Alcohol", 8.0, 15.0, 9.4)
+fixed_acidity = st.number_input("Fixed Acidity")
+volatile_acidity = st.number_input("Volatile Acidity")
+citric_acid = st.number_input("Citric Acid")
+residual_sugar = st.number_input("Residual Sugar")
+chlorides = st.number_input("Chlorides")
+free_sulfur_dioxide = st.number_input("Free Sulfur Dioxide")
+total_sulfur_dioxide = st.number_input("Total Sulfur Dioxide")
+density = st.number_input("Density")
+pH = st.number_input("pH")
+sulphates = st.number_input("Sulphates")
+alcohol = st.number_input("Alcohol")
 
 if st.button("Dự đoán chất lượng"):
     features = np.array([[fixed_acidity, volatile_acidity, citric_acid, residual_sugar, chlorides,
